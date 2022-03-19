2631b2848ad3be1fb39580d38adb3ba75c21ab11
Vexion <zakygames701@gmail.com>
2022-03-19 17:49:53 +0000

    [+] added the ability to change the colours used for server icons and pfp

--- noPictures/noPictures.plugin.js
+++ noPictures/noPictures.plugin.js
@@ -2,7 +2,7 @@
  * @name noPictures
  * @author DarkMatter
  * @description Removes all profile pictures and server pictures
- * @version 0.2.4
+ * @version 0.3.5
  * @authorId 200612445373464576
  * @authorLink https://bubblez.app/p?DarkMatter
  * @website https://bubblez.app/p?DarkMatter
@@ -17,7 +17,7 @@
             authors: [
                 {name: 'DarkMatter', discord_id: '200612445373464576', github_username: 'AFuxy', website: "https://bubblez.app/p?DarkMatter"}
             ],
-            version: '0.2.4',
+            version: '0.3.5',
             description: 'Removes all profile pictures and server pictures',
             github_raw: '',
             github: ''
@@ -28,7 +28,7 @@
             /* Added Changlog*/
             {
                 title: "Added", type: "added",
-                items: ["Initial Release", "All profile pictures and banners will be replaced with the colour red", "All server pictures will be replaced with the colour red"]
+                items: ["Initial Release", "All profile pictures and banners will be replaced with the colour decided in settings", "All server pictures will be replaced with the colour decided in settings"]
             },
 
 
@@ -40,16 +40,16 @@
 
 
             /* Fixed Changelog*/
-            // {
-            //     title: "Changed", type: "improved",
-            //     items: []
-            // }
+            {
+                title: "Changed", type: "improved",
+                items: ["You can now change the colour used for the pfp and banner", "You can now change the colour used for the server icon"]
+            },
 
 
             /* Progress Changelog*/
             {
                 title: "Working on", type: "progress",
-                items: ["Allowing you to customize the colour", "Emojies?"]
+                items: ["Allowing you to customize the colour - Done", "Emojies?"]
             }
 
 
@@ -57,12 +57,20 @@
         defaultConfig: [
 
 
-            /* Background settings */
+            /* Profile Picture settings */
         { 
-            type: 'category', id: 'picture', name: 'picture toggles', collapsible: false, shown: true,
+            type: 'category', id: 'picture', name: 'profile picture settings', collapsible: true, shown: false,
             settings: [
-                { type: "switch", id: "profiles", name: "Remove profile pictures", note: "Removes the profile picture of every user", value: true },
-                { type: "switch", id: "server", name: "Remove server pictures", note: "Removes the server picture of every server", value: true }
+                { type: "switch", id: "profiles", name: "Remove Profile Pictures", note: "Removes the profile picture of every user", value: true },
+                { type: "color", id: "colour", name: "Profile Colour Picker", note: "Change what colour users profile pictures will be replaced with", value: "#ff0000" },
+            ]
+        }, 
+            /* Server Icon settings */
+        { 
+            type: 'category', id: 'server', name: 'server icon settings', collapsible: true, shown: false,
+            settings: [
+                { type: "switch", id: "servers", name: "Remove Server Icon", note: "Removes the server icon from every server", value: true },
+                { type: "color", id: "colour", name: "Server Colour Picker", note: "Change what colour server icons will be replaced with", value: "#ff0000" }
             ]
         }, 
     ]
@@ -75,8 +83,8 @@ if (!global.ZeresPluginLibrary) {
 } else {
     return (([Plugin, Zlib]) => {
         const { WebpackModules, Patcher, ContextMenu, Logger: { error: Error } } = Zlib;
-        const profileCSS = "IMG.avatar-b5OQ1N {padding: 60px!important; background-color:red!important; width:0px!important; height: 0px!important;} div.banner-1YaD3N {background-color:red!important; background-image:none!important;} IMG.avatar-2e8lTP {padding: 20px!important; background-color:red!important; width:0px!important; height: 0px!important;} IMG.replyAvatar-sHd2sU {width:0px!important; height: 0px!important;}";
-        const serverCSS = "IMG.icon-3AqZ2e {padding: 70px!important; background-color:red!important; width:0px!important; height: 0px!important;} div.icon-1zKOXL, div.imageUploaderInner-IIRaFr {background-color:red!important; background-image:none!important;}";
+        // const profileCSS = "IMG.avatar-b5OQ1N {padding: 60px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} div.banner-1YaD3N {background-color:"+this.settings.picture.colour+"!important; background-image:none!important;} IMG.avatar-2e8lTP {padding: 20px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} IMG.replyAvatar-sHd2sU {width:0px!important; height: 0px!important;}";
+        // const serverCSS = "IMG.icon-3AqZ2e {padding: 70px!important; background-color:"+this.settings.server.colour+"!important; width:0px!important; height: 0px!important;} div.icon-1zKOXL, div.imageUploaderInner-IIRaFr {background-color:"+this.settings.server.colour+"!important; background-image:none!important;}";
         return class noPictures extends Plugin {
 
             /* ------------------------------------ ZLIB FUNCTIONS ------------------------------------ */
@@ -89,12 +97,12 @@ if (!global.ZeresPluginLibrary) {
             };
             onSwitch() {
                 if (this.settings.picture.profiles) {
-                    BdApi.injectCSS("profile", profileCSS);
+                    BdApi.injectCSS("profile", "IMG.avatar-b5OQ1N {padding: 60px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} div.banner-1YaD3N {background-color:"+this.settings.picture.colour+"!important; background-image:none!important;} IMG.avatar-2e8lTP {padding: 20px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} IMG.replyAvatar-sHd2sU {width:0px!important; height: 0px!important;}");
                 }else{
                     BdApi.clearCSS("profile");
                 }
-                if (this.settings.picture.server) {
-                    BdApi.injectCSS("server", serverCSS);
+                if (this.settings.server.servers) {
+                    BdApi.injectCSS("server", "IMG.icon-3AqZ2e {padding: 70px!important; background-color:"+this.settings.server.colour+"!important; width:0px!important; height: 0px!important;} div.icon-1zKOXL, div.imageUploaderInner-IIRaFr {background-color:"+this.settings.server.colour+"!important; background-image:none!important;}");
                 }else{
                     BdApi.clearCSS("server");
                 }
@@ -104,10 +112,10 @@ if (!global.ZeresPluginLibrary) {
                 /* ------------------------------------ PROFILE CONTENT PATCH ------------------------------------ */
                 // remove profile picture from every user
                 if (this.settings.picture.profiles) {
-                    BdApi.injectCSS("profile", profileCSS);
+                    BdApi.injectCSS("profile", "IMG.avatar-b5OQ1N {padding: 60px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} div.banner-1YaD3N {background-color:"+this.settings.picture.colour+"!important; background-image:none!important;} IMG.avatar-2e8lTP {padding: 20px!important; background-color:"+this.settings.picture.colour+"!important; width:0px!important; height: 0px!important;} IMG.replyAvatar-sHd2sU {width:0px!important; height: 0px!important;}");
                 }
-                if (this.settings.picture.server) {
-                    BdApi.injectCSS("server", serverCSS);
+                if (this.settings.server.servers) {
+                    BdApi.injectCSS("server", "IMG.icon-3AqZ2e {padding: 70px!important; background-color:"+this.settings.server.colour+"!important; width:0px!important; height: 0px!important;} div.icon-1zKOXL, div.imageUploaderInner-IIRaFr {background-color:"+this.settings.server.colour+"!important; background-image:none!important;}");
                 }
 
             }
